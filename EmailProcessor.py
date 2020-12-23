a64f73474da4f561bc016c8e572f3c1976a3d121
Alexy Skoutnev <Alexyskoutnev@gmail.com>
2020-12-23 17:03:36 -0600

    Read Email methods

--- EmailProcessor.py
+++ EmailProcessor.py
@@ -1,11 +1,22 @@
 import pandas as pd
+from collections import Counter
+
+def read_train():
+    spam = pd.read_csv('spam.csv', encoding='latin-1')
+    spam = spam.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1)
+    spam = spam.replace(['ham', 'spam'], [0, 1])
+    df = pd.DataFrame(columns=['crl.tot', 'dollar', 'bang', 'money', 'n000', 'make', 'free', 'spam'])
+    for x in spam.v2:
+        dataframe = email_decoder(x)
 
 
-# #pd.read_csv('spam.csv', encoding='latin-1')
-# # pd.read_csv('spam.csv', sep='|', encoding='latin-1')
-# df=pd.read_csv("spam.csv", encoding='ISO-8859-1')
-
-spam = pd.read_csv('spam.csv', encoding='latin-1')
-print(spam)
-spam = spam.drop(['Unnamed: 2','Unnamed: 3','Unnamed: 4'],axis=1)
-print(spam)
+def email_decoder(data, df):
+    capital_num = sum(1 for c in data if c.isupper())
+    dollar_count = data.count("$")
+    bang_count = data.count("!")
+    x = data.lower()
+    money_count = x.count("money")
+    zero_count = x.count("000")
+    make_count = x.count("make")
+    free_count = x.count("free")
+    pass
